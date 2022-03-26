8d0b8634aef06dc9b8acb5e9110f12f5bc45ab5b
Lorenz Koestler <lorenz@koestler.ch>
2022-03-26 22:37:20 +0100

    start to play around with websocket

--- src/components/Device.js
+++ src/components/Device.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Message, Notification, Table } from 'react-bulma-components'
 import HideableMessage from './HideableMessage'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -7,15 +7,33 @@ import { useAuth } from '../hooks/auth'
 import { unauthApi, useCategories, useValues } from '../hooks/unauthApi'
 import { Trans } from '@lingui/macro'
 import './Device.scss'
+import useWebSocket, { ReadyState } from 'react-use-websocket'
 
 const Device = ({ viewName, viewIsPublic, deviceName, deviceTitle }) => {
+  // const { play } = useContext(AutoplayContext)
   const { api } = useAuth()
   const { categories, success: rSuccess, error: rError } = useCategories(viewIsPublic ? unauthApi : api, viewName, deviceName)
   const { values, success: vSuccess, error: vError } = useValues(viewIsPublic ? unauthApi : api, viewName, deviceName)
 
+  const { sendMessage, lastMessage, readyState } = useWebSocket(`ws://localhost:8000/api/v1/values/${viewName}/${deviceName}.ws`)
+
+  // create websock connection
+  useEffect(() => {
+    console.log('sendMessage', sendMessage)
+  }, [sendMessage])
+
+  useEffect(() => {
+    console.log('lastMessage', lastMessage)
+  }, [lastMessage])
+
+  useEffect(() => {
+    console.log('readyState', readyState)
+  }, [readyState])
+
   return (
     <HideableMessage header={<p>{deviceTitle}</p>}>
       <Message.Body>
+        <Notification>Websocket State: <ConnectionState readyState={readyState} /></Notification>
         {rError && <Notification color='danger'><Trans>Cannot load device registers.</Trans></Notification>}
         {vError && <Notification color='danger'><Trans>Cannot load device values.</Trans></Notification>}
         {rSuccess && vSuccess && <ConfiguredDevice viewName={viewName} deviceName={deviceName} categories={categories} values={values} />}
@@ -24,10 +42,18 @@ const Device = ({ viewName, viewIsPublic, deviceName, deviceTitle }) => {
   )
 }
 
-const ConfiguredDevice = ({ viewName, deviceName, categories, values }) => {
-  // separate registers into categories
-  console.log('categories', categories)
+const ConnectionState = ({ readyState }) => {
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'Connecting',
+    [ReadyState.OPEN]: 'Open',
+    [ReadyState.CLOSING]: 'Closing',
+    [ReadyState.CLOSED]: 'Closed',
+    [ReadyState.UNINSTANTIATED]: 'Uninstantiated'
+  }
+  return connectionStatus[readyState]
+}
 
+const ConfiguredDevice = ({ viewName, deviceName, categories, values }) => {
   return (
     <Table className='device'>
       <tbody>
