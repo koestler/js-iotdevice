4ad1d9affe4f963907633ceb7809d567df08575f
Lorenz Koestler <lorenz@koestler.ch>
2022-04-13 23:18:17 +0200

    only blink led when values changed

--- src/components/Led.js
+++ src/components/Led.js
@@ -3,10 +3,11 @@ import React, { useState, useEffect } from 'react'
 import './Les.scss'
 
 const Led = ({ blinkOnChange }) => {
+  const d = JSON.stringify(blinkOnChange)
   const [key, setKey] = useState(0)
   useEffect(() => {
     setKey(key => key + 1)
-  }, [blinkOnChange, setKey])
+  }, [d, setKey])
   // key is changed in order to re-create the dom element and thereby restart the css animation
   return <div key={key} className='led' />
 }
