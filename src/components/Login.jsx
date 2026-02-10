8bf05fa38854da68563fb41ead255c6393dc71d5
Lorenz Koestler <lorenz@koestler.ch>
2026-02-10 17:52:06 +0100

    remove react-hook-form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Box, Form, Section, Heading } from 'react-bulma-components'
-import { useForm } from 'react-hook-form'
 import { useAuth } from '../hooks/auth'
 import { useLogin } from '../hooks/unauthApi'
 import { t } from '@lingui/core/macro'
@@ -8,29 +7,49 @@ import { Trans } from '@lingui/react/macro'
 import { toast } from 'bulma-toast'
 
 const Login = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm()
+  const [user, setUser] = useState('')
+  const [password, setPassword] = useState('')
+  const [errors, setErrors] = useState({})
   const { logout } = useAuth()
   const { login } = useLogin({
     onSucces: user => toast({ message: t`You have been logged in as ${user}.`, type: 'is-success' }),
     onError: () => toast({ message: t`Login failed`, type: 'is-danger' })
   })
 
-  const onSubmit = async data => {
-    logout()
-    login(data.user, data.password)
+  const validateForm = () => {
+    const newErrors = {}
+    if (!user || user.length < 2) {
+      newErrors.user = true
+    }
+    if (!password || password.length < 4) {
+      newErrors.password = true
+    }
+    return newErrors
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    const validationErrors = validateForm()
+    setErrors(validationErrors)
+
+    if (Object.keys(validationErrors).length === 0) {
+      logout()
+      login(user, password)
+    }
   }
 
   return (
     <Section>
       <Heading renderAs='h2'>Log in</Heading>
       <Box style={{ maxWidth: 600, margin: 'auto' }}>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={onSubmit}>
           <Form.Field>
             <Form.Label><Trans>User</Trans></Form.Label>
             <input
               type='text'
               className={'input is-primary' + (errors.user ? ' is-danger' : '')}
-              {...register('user', { required: true, minLength: 2 })}
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
             />
           </Form.Field>
           <Form.Field>
@@ -38,7 +57,8 @@ const Login = () => {
             <input
               type='password'
               className={'input is-primary' + (errors.password ? ' is-danger' : '')}
-              {...register('password', { required: true, minLength: 4 })}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </Form.Field>
           <Button.Group align='right'>
